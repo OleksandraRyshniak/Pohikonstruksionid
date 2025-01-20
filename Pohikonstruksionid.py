2f395e6aa0e912f5034796dca76224565d739ff3
opilane <opilane@HP-CZC2349HV2>
2025-01-20 11:28:43 +0200

    print funksion 20.01.2025

--- Pohikonstruksionid.py
+++ Pohikonstruksionid.py
@@ -9,22 +9,22 @@ aste=2
 tulemus=a**aste
 print(a,"**",aste,"=",tulemus)
 # korrutamine *
-#arv1=int(input("Esimene korrutaja: "))
-#arv2=int(input("Teine korrutaja: "))
-#tulemus=arv1*arv2
-#print(arv1,"*",arv2,"=",tulemus)
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Teine korrutaja: "))
+tulemus=arv1*arv2
+print(arv1,"*",arv2,"=",tulemus)
 #jagamine /
-#arv1=int(input("Esimene korrutaja: "))
-#arv2=int(input("Jagaja: "))
-#tulemus=arv1/arv2
-#print(arv1,"/",arv2,"=",tulemus)
-#print(type(tulemus))
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Jagaja: "))
+tulemus=arv1/arv2
+print(arv1,"/",arv2,"=",tulemus)
+print(type(tulemus))
 #jagamisjääk %
-#arv1=int(input("Esimene korrutaja: "))
-#arv2=int(input("Jagaja: "))
-#tulemus=arv1%arv2
-#print(arv1,"%",arv2,"=",tulemus)
-#print(type(tulemus))
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Jagaja: "))
+tulemus=arv1%arv2
+print(arv1,"%",arv2,"=",tulemus)
+print(type(tulemus))
 #liitmine +
 arv1=int(input("Esimene korrutaja: "))
 arv2=int(input("Teine korrutaja: "))
@@ -36,3 +36,8 @@ arv1=int(input("Esimene korrutaja: "))
 arv2=int(input("Teine korrutaja: "))
 tulemus=arv1-arv2
 print(arv1,"-",arv2,"=",tulemus)
+# jagamise täisarvulise osa leidmine //
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Jagaja: "))
+tulemus=arv1//arv2
+print(arv1,"//",arv2,"=",tulemus)
