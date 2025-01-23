b5223441e04958439c393cb6538aa0f1e8d0cc15
opilane <opilane@HP-CZC2349HV2>
2025-01-23 13:38:12 +0200

    fg

---
+++ module1.py
@@ -0,0 +1,20 @@
+print("Tere mailm!")
+nimi=input("Mis on sinu nimi?").capitalize()
+print(f"Tere maailm!, Tervitan sind {nimi}")
+vanus=int(input("Kui vana sa oled?"))
+print(f"Tere maailm!, Tervitan sind {nimi}! Sa oled {vanus} aasta vana. ")
+## 2
+#vanus = 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#kas_käib_koolis = True
+#print(f"Muutuja {vanus} on {type(vanus)}")
+#print(f"Muutuja {eesnimi} on {type(eesnimi)}")
+#print(f"Muutuja {pikkus} on {type(pikkus)}")
+#print(f"Muutuj {kas_käib_koolis} on {type(kas_käib_koolis)}")
+# 3
+#kommide_arv=randint(10,100)
+#print(f"Laua peal on {kommide_arv}")
+#mitu=int(input("Mitu tahad süüja"))
+#print(f"Laua peal on jäänud {kommide_arv-mitu}")
+# 4
